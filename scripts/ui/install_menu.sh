4526a2dcdf1e57c1578aec451a8a943b94e760e9
lodge <shilong.native@gmail.com>
2022-08-28 17:44:21 +0800

    merge

--- scripts/ui/install_menu.sh
+++ scripts/ui/install_menu.sh
@@ -12,7 +12,7 @@
 set -e
 
 function install_ui() {
-<<<<<<< HEAD
+
     top_border
     echo -e "|     ${green}~~~~~~~~~~~ [ Installation Menu ] ~~~~~~~~~~~${white}     |"
     hr
@@ -20,44 +20,29 @@ function install_ui() {
     echo -e "|  all necessary dependencies for the various           |"
     echo -e "|  functions on a completely fresh system.              |"
     hr
-    echo -e "| Firmware & API:           | 3rd Party Webinterface:   |"
-    echo -e "|  1) [Klipper]             |  6) [OctoPrint]           |"
-    echo -e "|  2) [Moonraker]           |                           |"
-    echo -e "|                           | Other:                    |"
-    echo -e "| Klipper Webinterface:     |  7) [PrettyGCode]         |"
-    echo -e "|  3) [Mainsail]            |  8) [Telegram Bot]        |"
-    echo -e "|  4) [Fluidd]              |                           |"
-    echo -e "|                           | Webcam Streamer:          |"
-    echo -e "| Touchscreen GUI:          |  9) [MJPG-Streamer]       |"
-    echo -e "|  5) [KlipperScreen]       |                           |"
+    echo -e "| Firmware & API:          | 3rd Party Webinterface:    |"
+    echo -e "|  1) [Klipper]            |  6) [OctoPrint]            |"
+    echo -e "|  2) [Moonraker]          |                            |"
+    echo -e "|                          | Other:                     |"
+    echo -e "| Klipper Webinterface:    |  7) [PrettyGCode]          |"
+    echo -e "|  3) [Mainsail]           |  8) [Telegram Bot]         |"
+    echo -e "|  4) [Fluidd]             |  9) $(obico_install_title) |"
+    echo -e "|                          |                            |"
+    echo -e "| Touchscreen GUI:         | Webcam Streamer:           |"
+    echo -e "|  5) [KlipperScreen]      | 10) [MJPG-Streamer]        |"
     back_footer
-=======
-  top_border
-  echo -e "|     ${green}~~~~~~~~~~~ [ Installation Menu ] ~~~~~~~~~~~${white}     |"
-  hr
-  echo -e "|  You need this menu usually only for installing       |"
-  echo -e "|  all necessary dependencies for the various           |"
-  echo -e "|  functions on a completely fresh system.              |"
-  hr
-  echo -e "| Firmware & API:          | 3rd Party Webinterface:    |"
-  echo -e "|  1) [Klipper]            |  6) [OctoPrint]            |"
-  echo -e "|  2) [Moonraker]          |                            |"
-  echo -e "|                          | Other:                     |"
-  echo -e "| Klipper Webinterface:    |  7) [PrettyGCode]          |"
-  echo -e "|  3) [Mainsail]           |  8) [Telegram Bot]         |"
-  echo -e "|  4) [Fluidd]             |  9) $(obico_install_title) |"
-  echo -e "|                          |                            |"
-  echo -e "| Touchscreen GUI:         | Webcam Streamer:           |"
-  echo -e "|  5) [KlipperScreen]      | 10) [MJPG-Streamer]        |"
-  back_footer
->>>>>>> master
+
 }
 
 function install_menu() {
     clear && print_header
     install_ui
 
-<<<<<<< HEAD
+    ### save all installed webinterface ports to the ini file
+    fetch_webui_ports
+    ### save all klipper multi-instance names to the ini file
+    set_multi_instance_names
+
     local action
     while true; do
         read -p "${cyan}####### Perform action:${white} " action
@@ -79,6 +64,8 @@ function install_menu() {
             8)
                 do_action "telegram_bot_setup_dialog" "install_ui";;
             9)
+                do_action "moonraker_obico_setup_dialog" "install_ui";;
+            10)
                 do_action "install_mjpg-streamer" "install_ui";;
             B|b)
                 clear; main_menu; break;;
@@ -87,45 +74,6 @@ function install_menu() {
         esac
     done
     install_menu
-=======
-  ### save all installed webinterface ports to the ini file
-  fetch_webui_ports
-
-  ### save all klipper multi-instance names to the ini file
-  set_multi_instance_names
-
-  local action
-  while true; do
-    read -p "${cyan}####### Perform action:${white} " action
-    case "${action}" in
-      1)
-        do_action "select_klipper_python_version" "install_ui";;
-      2)
-        do_action "moonraker_setup_dialog" "install_ui";;
-      3)
-        do_action "install_mainsail" "install_ui";;
-      4)
-        do_action "install_fluidd" "install_ui";;
-      5)
-        do_action "install_klipperscreen" "install_ui";;
-      6)
-        do_action "octoprint_setup_dialog" "install_ui";;
-      7)
-        do_action "install_pgc_for_klipper" "install_ui";;
-      8)
-        do_action "telegram_bot_setup_dialog" "install_ui";;
-      9)
-        do_action "moonraker_obico_setup_dialog" "install_ui";;
-      10)
-        do_action "install_mjpg-streamer" "install_ui";;
-      B|b)
-        clear; main_menu; break;;
-      *)
-        deny_action "install_ui";;
-    esac
-  done
-  install_menu
->>>>>>> master
 }
 
 function select_klipper_python_version() {
